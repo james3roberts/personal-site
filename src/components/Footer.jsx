dfd4d8567fd3cba62c6ef42053cbca5a9483980c
James <james3roberts@gmail.com>
2025-11-19 04:17:19 -0700

    changed the name on the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-white shadow p-4 text-center text-gray-500 text-sm mt-auto">
-      © {new Date().getFullYear()} DKS Works. All rights reserved.
+      © {new Date().getFullYear()} DKS. All rights reserved.
     </footer>
   );
 }
