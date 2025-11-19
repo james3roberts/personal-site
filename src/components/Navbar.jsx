ec9f6c1a46ea720cd9ff7d6818c9f913d0d430ef
James <james3roberts@gmail.com>
2025-11-19 05:14:44 -0700

    updated color in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ export default function Navbar() {
         {/* Logo */}
         <div className="flex flex-col leading-none">
           <div className="text-2xl font-bold text-blue-700">DKS</div>
-          <div className="text-xs font-bold text-blue-700 -mt-1">DUMB KIDS SOCIETY</div>  {/* example */}
+          <div className="text-xs font-bold text-blue-850 -mt-1">DUMB KIDS SOCIETY</div>  {/* example */}
         </div>
 
         {/* Desktop Nav */}
