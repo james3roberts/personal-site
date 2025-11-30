de78468f770402af1c4986aee715b8dd89c33a3a
James <james3roberts@gmail.com>
2025-11-29 20:12:05 -0700

    trying to fix my import mess

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import { Routes, Route } from "react-router-dom";
+
+// Main pages
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
+
+// Client demo pages
 import ClientDemo from "./pages/ClientDemo";
 import Landing from "./pages/ClientDemo/landing";
 import Basic from "./pages/ClientDemo/basic";
-import ProLevel from "./pages/ClientDemo/prolevel"; // ✅ Fixed path with correct case
+import ProLevel from "./pages/ClientDemo/prolevel";
 
+// Pricing
 import Prices from "./pages/Prices";
+
+// Legal pages (FIXED imports)
+import Privacy from "./pages/Privacy";
+import Terms from "./pages/Terms";
+
+// Layout
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
@@ -16,21 +27,30 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col">
       <Navbar />
+
       <main className="p-4 flex-grow">
         <Routes>
+          {/* Main */}
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
+
+          {/* Client Demo */}
           <Route path="/clientDemo" element={<ClientDemo />} />
           <Route path="/clientDemo/landing" element={<Landing />} />
           <Route path="/clientDemo/basic" element={<Basic />} />
-          <Route path="/clientDemo/ProLevel" element={<ProLevel />} />
+          <Route path="/clientDemo/prolevel" element={<ProLevel />} />
+
+          {/* Prices */}
           <Route path="/prices" element={<Prices />} />
+
+          {/* Legal (FIXED) */}
           <Route path="/privacy" element={<Privacy />} />
           <Route path="/terms" element={<Terms />} />
         </Routes>
       </main>
+
       <Footer />
     </div>
   );
